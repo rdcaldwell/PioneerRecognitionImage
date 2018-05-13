a0bd9045c413d30fbf4d0e84e462e6cf134fb840
rdcaldwell <rdcald@yahoo.com>
2018-05-12 20:46:34 -0400

    Add interval

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, request, jsonify
 
 import recognizer
 import os
+import threading
+import requests
 
 dir = os.path.dirname(__file__)
-
 app = Flask(__name__, instance_relative_config=True)
+interval = 300
 
 
 @app.route('/api/image/recognize', methods=['POST'])
@@ -22,5 +24,11 @@ def recognize():
     return jsonify(json)
 
 
+def start_timer():
+    threading.Timer(interval, start_timer).start()
+    print('Pinging heroku...')
+    requests.get('https://imagerecognitionrobot.herokuapp.com')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+    start_timer()
