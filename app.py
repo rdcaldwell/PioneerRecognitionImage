3281a27ee058dc4f988fcfc2cd48791e4e6d6be5
rdcaldwell <rdcald@yahoo.com>
2018-05-12 20:58:35 -0400

    fix

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask, request, jsonify
 
 import recognizer
 import os
-import threading
-import requests
 
 dir = os.path.dirname(__file__)
+
 app = Flask(__name__, instance_relative_config=True)
-interval = 300
 
 
 @app.route('/api/image/recognize', methods=['POST'])
@@ -24,11 +22,5 @@ def recognize():
     return jsonify(json)
 
 
-def start_timer():
-    threading.Timer(interval, start_timer).start()
-    print('Pinging heroku...')
-    requests.get('https://imagerecognitionrobot.herokuapp.com')
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-    start_timer()
